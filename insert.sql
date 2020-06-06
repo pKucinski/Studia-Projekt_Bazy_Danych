54350eeaacb56d8201375a7d18f3d3cd91e5f68a
Patryk Kuciński <pkucinski@MacBook-Air-Patryk.local>
2020-06-06 12:41:07 +0200

    fix_insert

--- insert.sql
+++ insert.sql
@@ -84,7 +84,6 @@ INSERT INTO Umiejetnosc_Specjalna (id, Dodatkowe_Obrazenia, Czas_Odnowienia) VAL
 INSERT INTO Umiejetnosc_Specjalna (id, Dodatkowe_Obrazenia, Czas_Odnowienia) VALUES (10, 10, 45);
 INSERT INTO Umiejetnosc_Specjalna (id, Dodatkowe_Obrazenia, Czas_Odnowienia) VALUES (11, 5, 20);
 INSERT INTO Umiejetnosc_Specjalna (id, Dodatkowe_Obrazenia, Czas_Odnowienia) VALUES (12, 3, 5);
-INSERT INTO Umiejetnosc_Specjalna (id, Dodatkowe_Obrazenia, Czas_Odnowienia) VALUES (13, 3, 15);
 INSERT INTO Umiejetnosc_Specjalna (id, Dodatkowe_Obrazenia, Czas_Odnowienia) VALUES (13, 1, 3);
 INSERT INTO Umiejetnosc_Specjalna (id, Dodatkowe_Obrazenia, Czas_Odnowienia) VALUES (14, 6, 15);
 INSERT INTO Umiejetnosc_Specjalna (id, Dodatkowe_Obrazenia, Czas_Odnowienia) VALUES (15, 8, 15);
@@ -100,85 +99,3 @@ INSERT INTO Umiejetnosc_Specjalna (id, Dodatkowe_Obrazenia, Czas_Odnowienia) VAL
 INSERT INTO Umiejetnosc_Specjalna (id, Dodatkowe_Obrazenia, Czas_Odnowienia) VALUES (25, 3, 15);
 INSERT INTO Umiejetnosc_Specjalna (id, Dodatkowe_Obrazenia, Czas_Odnowienia) VALUES (26, 5, 40);
 INSERT INTO Umiejetnosc_Specjalna (id, Dodatkowe_Obrazenia, Czas_Odnowienia) VALUES (27, 13, 60);
-
-
-
-CREATE TABLE Krolestwo (
-	id int NOT NULL,
-	Nazwa varchar(255) NOT NULL,
-	Krol varchar(255) NOT NULL,
-	Nastepca varchar(255),
-	CONSTRAINT Krolestwo_pk PRIMARY KEY (id)
-);
-
-CREATE TABLE Zamek (
-	id int NOT NULL,
-	Nazwa varchar(255) NOT NULL,
-	Zloto int NOT NULL,
-	Drewno int NOT NULL,
-	Krolestwo_id int NOT NULL,
-	CONSTRAINT Zamek_pk PRIMARY KEY (id),
-	FOREIGN KEY (Krolestwo_id) REFERENCES Krolestwo(id) ON DELETE CASCADE
-);
-
-
-CREATE TABLE Kopalnia_Zlota (
-	id int NOT NULL,
-	Nazwa varchar(255) NOT NULL,
-	Zamek_id int,
-	CONSTRAINT kopalnia_zlota_pk PRIMARY KEY (id),
-	FOREIGN KEY (Zamek_id) REFERENCES Zamek(id) ON DELETE CASCADE
-);
-
-
-
-CREATE TABLE Tartak (
-	id int NOT NULL,
-	Nazwa varchar(255) NOT NULL,
-	Zamek_id int,
-	CONSTRAINT Tartak_pk PRIMARY KEY (id),
-	FOREIGN KEY (Zamek_id) REFERENCES Zamek(id) ON DELETE CASCADE
-);
-
-
-
-CREATE TABLE Potwory (
-	id int NOT NULL,
-	Zycie int NOT NULL,
-	Atak int NOT NULL,
-	Obrona int NOT NULL,
-	CONSTRAINT Potwory_pk PRIMARY KEY (id)
-);
-
-
-CREATE TABLE Krolestwo_Potwory (
-	Krolestwo_id int NOT NULL,
-	Powtowry_id int NOT NULL,
-	PRIMARY KEY (Krolestwo_id, Powtowry_id),
-	FOREIGN KEY (Krolestwo_id) REFERENCES Krolestwo(id) ON UPDATE CASCADE,
-  FOREIGN KEY (Powtowry_id) REFERENCES Potwory(id) ON UPDATE CASCADE
-);
-
-
-CREATE TABLE Rycerze (
-	id int NOT NULL,
-	Nazwa varchar(255) NOT NULL,
-	Atak int NOT NULL,
-	Obrona int NOT NULL,
-	Zycie int NOT NULL,
-	Krolestwo_id int NOT NULL,
-	Zamek_id int NOT NULL,
-	CONSTRAINT Rycerze_pk PRIMARY KEY (id),
-	FOREIGN KEY (Zamek_id) REFERENCES Zamek(id) ON DELETE CASCADE,
-	FOREIGN KEY (Krolestwo_id) REFERENCES Krolestwo(id) ON DELETE CASCADE
-);
-
-
-
-CREATE TABLE Umiejetnosc_Specjalna (
-	id int NOT NULL,
-	Dodatkowe_Obrazenia int NOT NULL,
-	Czas_Odnowienia int NOT NULL,
-	CONSTRAINT Umiejetność_Specjalna_pk PRIMARY KEY (id),
-	CONSTRAINT fk_rycerz_id FOREIGN KEY (id) REFERENCES Rycerze (id)
-);
